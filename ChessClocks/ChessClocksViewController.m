37bfc2f969d5a445c5a6cd6d1ddeed6ddf6d2c02
Adam Williams <adam@thewilliams.ws>
2011-06-03 12:16:16 -0400

    Showing overlay message before starting clocks.

--- ChessClocks/ChessClocksViewController.m
+++ ChessClocks/ChessClocksViewController.m
@@ -8,6 +8,7 @@
 @property (nonatomic, retain) PlayerClock *playerClockOne;
 @property (nonatomic, retain) PlayerClock *playerClockTwo;
 @property (nonatomic, retain) PlayerClock *currentPlayerClock;
+@property (nonatomic, assign) UILabel *overlayMessageLabel;
 
 - (void)showPauseGameActionSheet;
 - (void)showNewGameView;
@@ -24,23 +25,46 @@
 @synthesize playerClockOne, playerClockTwo, currentPlayerClock;
 @synthesize playerOneTimeLabel, playerTwoTimeLabel;
 @synthesize playerOneBackgroundImage, playerTwoBackgroundImage;
+@synthesize overlayMessageLabel;
+
+
+#pragma mark
+#pragma mark Overlay
+
+- (void) showOverlayMessage:(NSString *)message
+{
+  self.overlayMessageLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 200, 280, 100)];
+  self.overlayMessageLabel.text = message;
+  self.overlayMessageLabel.numberOfLines = 2;
+  [[self.view window] insertSubview:self.overlayMessageLabel aboveSubview:self.view];  
+}
+
+
+- (void) hideOverlayMessage
+{
+  [self.overlayMessageLabel removeFromSuperview];
+  self.overlayMessageLabel = nil;
+}
 
 
 #pragma mark
 #pragma mark Pause Game
 
-- (IBAction)pauseGame:(id) sender {
+- (IBAction)pauseGame:(id) sender
+{
   [self stopClocks];
   [self showPauseGameActionSheet];
 }
 
-- (void)showPauseGameActionSheet {
+- (void)showPauseGameActionSheet
+{
   UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Paused" delegate:self cancelButtonTitle:@"Resume" destructiveButtonTitle:nil otherButtonTitles:@"New Game", nil];
   [actionSheet showInView:self.view];
   [actionSheet release];
 }
 
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
   switch (buttonIndex) {
     case 0: // New Game
       [self showNewGameView];
@@ -55,7 +79,8 @@
 #pragma mark
 #pragma mark New Game
 
-- (void)showNewGameView {
+- (void)showNewGameView
+{
   [[UIApplication sharedApplication]setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
   NewGameViewController *newGameController = [[NewGameViewController alloc]initWithNibName:@"NewGameViewController" bundle:nil];
   newGameController.delegate = self;
@@ -63,17 +88,20 @@
   [newGameController release];
 }
 
-- (void)hideNewGameView {
+- (void)hideNewGameView
+{
   [[UIApplication sharedApplication]setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
   [self dismissModalViewControllerAnimated:YES];
 }
 
-- (void)newGameViewController:(id)newGameViewController didStart:(ClockTime *)clockTime {
+- (void)newGameViewController:(id)newGameViewController didStart:(ClockTime *)clockTime
+{
   [self hideNewGameView];
   [self startGameWithTime:clockTime];
 }
 
-- (void)newGameViewControllerDidCancel:(id)newGameViewController {
+- (void)newGameViewControllerDidCancel:(id)newGameViewController
+{
   [self hideNewGameView];
   [self showPauseGameActionSheet];
 }
@@ -82,7 +110,13 @@
 #pragma mark
 #pragma mark Game
 
-- (void)setCurrentPlayerClock:(PlayerClock *)clock {
+- (BOOL)isGameStarted
+{
+  return self.overlayMessageLabel == nil;
+}
+
+- (void)setCurrentPlayerClock:(PlayerClock *)clock
+{
   static NSString *keyPath = @"clockTime";
   
   if (currentPlayerClock != nil) {
@@ -98,32 +132,39 @@
   [currentPlayerClock addObserver:self forKeyPath:keyPath options:0 context:nil];
 }
 
-- (void)startGameWithTime:(ClockTime *) time {
+- (void)startGameWithTime:(ClockTime *) time
+{
   self.playerClockOne = [PlayerClock clockWithTime:time];
   self.playerClockTwo = [PlayerClock clockWithTime:time];
   self.currentPlayerClock = playerClockOne;
-  [self startClocks];
+  [self updateClockDisplay];
+
+  [self showOverlayMessage: @"Tap anywhere to start.\nTap again to switch clocks."];
 }
 
-- (void)updateClockDisplay {
+- (void)updateClockDisplay
+{
   playerOneTimeLabel.text = playerClockOne.clockTime.string;
   playerTwoTimeLabel.text = playerClockTwo.clockTime.string;
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
   [self updateClockDisplay];
 }
 
-- (void)startClocks {
-  [self updateClockDisplay];
+- (void)startClocks
+{
   [currentPlayerClock startCountdown];
 }
 
-- (void)stopClocks {
+- (void)stopClocks
+{
   [currentPlayerClock stopCountdown];
 }
 
-- (void) toggleCurrentPlayerClock {
+- (void) toggleCurrentPlayerClock
+{
   [currentPlayerClock stopCountdown];
   if (playerClockOne == currentPlayerClock) {
     self.currentPlayerClock = playerClockTwo;
@@ -133,8 +174,14 @@
   [currentPlayerClock startCountdown];
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-  [self toggleCurrentPlayerClock];
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  if ([self isGameStarted]) {
+    [self toggleCurrentPlayerClock];
+  } else {
+    [self hideOverlayMessage];
+    [self startClocks];
+  }
 }
 
 @end
